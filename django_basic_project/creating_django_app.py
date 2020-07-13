7ab71cd40b14a63b0cd6a51c5c94fc3cb095d96f
vigneshMSD07 <68139995+vigneshMSD07@users.noreply.github.com>
2020-07-13 19:29:19 +0530

    test

--- django_basic_project/creating_django_app.py
+++ django_basic_project/creating_django_app.py
@@ -1,6 +1,6 @@
 Creating a django application
 
-why app is required?
+why django app is required?
 Django app are used to perform a particular action in the webpage.
 Eg: regristration app, polling app etc.
 THe app is independent of django projects so it can be used in other projects also.
