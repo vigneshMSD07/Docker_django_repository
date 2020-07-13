8aa1d27c707612282f9e9fa195e09284c1179c7f
vigneshMSD07 <68139995+vigneshMSD07@users.noreply.github.com>
2020-07-13 19:11:51 +0530

    fix

---
+++ django_basic_project/creating_django_app.py
@@ -0,0 +1,10 @@
+Creating a django application
+
+why app is required?
+Django app are used to perform a particular action in the webpage.
+Eg: regristration app, polling app etc.
+THe app is independent of django projects so it can be used in other projects also.
+
+Steps to create a app:
+Step 1. python manage.py startapp <app_name>
+Step 2. Add the app name in the INSTALLED_APPS list inside the settings.py in django project
