c46bd28fd776dcbfd4208120b3abc94af28f78e9
vigneshMSD07 <68139995+vigneshMSD07@users.noreply.github.com>
2020-08-05 14:32:30 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,17 @@
 print "welcome to first pipeline execution"
+
+import argparse
+import ast
+
+def test(args):
+  for i in args.versions:
+    print i
+  
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-vl','--versions', help='name_of_script', type=ast.literal_eval)
+  args= parser.parse_args()
+  test(args)
+  
+if __name__ == "__main__":
+  main()
