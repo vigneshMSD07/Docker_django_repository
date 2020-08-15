1bcae4f11bc77681613f86e2c5cfc6b1038d59f7
vigneshMSD07 <68139995+vigneshMSD07@users.noreply.github.com>
2020-08-15 12:24:41 +0530

    fix

--- Docker_files/docker-service.sh
+++ Docker_files/docker-service.sh
@@ -6,10 +6,10 @@ docker service create <image_name>
 eg: docker service create nginx:latest
 
 #command to add port to the services
-docker service --publish-add <port> <service id>
+docker service update --publish-add <port> <service id>
 
 #command to add replicawhich is similar to scalling in compose
-docker service --replicas 4 <service id> (this will replicate 4 times the service)
+docker service update --replicas 4 <service id> (this will replicate 4 times the service)
 
 #command to list all services:
 docker service ls
